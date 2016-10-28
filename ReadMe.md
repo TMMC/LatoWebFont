e1643f2904d284fd612a0f8e1f5ebf38df7d9089
TMMC <TMMC@users.noreply.github.com>
2016-10-28 11:46:08 +0200

    Config and structure changes
    Edited git related configs, added gulp configs, restructured dirs.

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,3 @@
 # Lato Web Fonts
 The repository purpose is usage of [Lato Font](http://www.latofonts.com/lato-free-fonts/) designed by Łukasz Dziedzic.  
-The directory `fonts` contains fonts from http://www.latofonts.com/download/Lato2OFLWeb.zip `Lato` subfolder (webfonts with the complete character set).
-
-## TO DO
-Less and Sass.
+The directory `dist/fonts` contains webfonts with complete character set from `Lato` subfolder in [official Lato2OFLWeb.zip](http://www.latofonts.com/download/Lato2OFLWeb.zip).
---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// ==============================================
+// === Require & plugins config
+// ==============================================
+var gulp               = require('gulp'),
+  plugins              = require('gulp-load-plugins')({
+    pattern : ['gulp-*', 'gulp.*', 'less-*'],
+    rename  : {'less-plugin-autoprefix': 'lessAutoprefix', 'less-plugin-clean-css' : 'lessCleanCss'}
+  }),
+  autoprefixerOptions  = { browsers: [ 'last 3 versions', '> 5%', 'ie >= 8', 'firefox >= 4', 'android >= 4' ], cascade: true },
+  lessAutoprefixer     = new plugins.lessAutoprefix(autoprefixerOptions),
+  cleanCssOptions      = {advanced: true, compatibility: ["ie8"], s1: true},
+  lessCleanCss         = new plugins.lessCleanCss(cleanCssOptions),
+  sassOptionsDev       = { errLogToConsole: false, outputStyle: 'expanded' },
+  sassOptionsProd      = { errLogToConsole: false, outputStyle: 'compressed' },
+  lessOptionsDev       = { plugins: [lessAutoprefixer], ieCompat: true, compress: false, sourceMap: false },
+  lessOptionsProd      = { plugins: [lessAutoprefixer, lessCleanCss], ieCompat: true, compress: true, sourceMap: false },
+  cssCompiler          = 'sass',
+  dateYear             = new Date().getFullYear();
+
+// ==============================================
+// === Tasks
+// ==============================================
+
+// == compile-sass task: compiling sass to css
+gulp.task('compile-sass', function() {
+  // Outputs non-minified and minified stylesheets
+  return gulp.src('src/fonts.scss')
+    .pipe(plugins.plumber())
+    .pipe(plugins.sass(sassOptionsDev))
+    .on('error', function (err) { plugins.util.log(err); this.emit('end'); })
+    .pipe(plugins.autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(plugins.sass(sassOptionsProd))
+    .on('error', function (err) { plugins.util.log(err); this.emit('end'); })
+    .pipe(plugins.rename({ extname : '.min.css' }))
+    .pipe(plugins.autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('dist/css'));
+});
+
+// == compile-less task: compiling less to css
+gulp.task('compile-less', function () {
+  return gulp.src('src/fonts.less')
+    .pipe(plugins.plumber())
+    .pipe(plugins.less(lessOptionsDev))
+    .on('error', function (err) { plugins.util.log(err); this.emit('end'); })
+    .pipe(gulp.dest('dist/css'))
+    .pipe(plugins.less(lessOptionsProd))
+    .on('error', function (err) { plugins.util.log(err); this.emit('end'); })
+    .pipe(plugins.rename({ extname : '.min.css' }))
+    .pipe(gulp.dest('dist/css'));
+    gulp.watch('src/fonts.less', ['build-css']);
+});
+
+// == build-css task: compiling css depending on preprocessor
+if (cssCompiler === 'sass') {
+  gulp.task('build-css', ['compile-sass']);
+} else {
+  gulp.task('build-css', ['compile-less']);
+}
+
+// == watch task
+gulp.task('watch', function() {
+  if (cssCompiler === 'sass') {
+    gulp.watch('src/fonts.scss', ['compile-sass']);
+  } else {
+    gulp.watch('src/fonts.less', ['compile-less']);
+  }
+});
+
+// == default task
+gulp.task('default', ['build-css', 'watch']);
